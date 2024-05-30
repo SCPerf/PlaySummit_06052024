3f65efdf33004f28e1a86303968c04062ac00d48
John Montojo <11521672+jjrm@users.noreply.github.com>
2024-05-30 13:57:39 -0400

    Update next.config.js

--- src/rendering/next.config.js
+++ src/rendering/next.config.js
@@ -86,16 +86,13 @@ const nextConfig = {
 };
 
 module.exports = () => {
-  // Run the base config through any configured plugins
-  return Object.values(plugins).reduce((acc, plugin) => plugin(acc), nextConfig);
-};
-
-module.exports = {
   // In order for newrelic to effectively instrument a Next.js application,
   // the modules that newrelic supports should not be mangled by webpack. Thus,
   // we need to "externalize" all of the modules that newrelic supports.
   webpack: (config) => {
     nrExternals(config)
     return config
-  }
-}
+  },
+  // Run the base config through any configured plugins
+  return Object.values(plugins).reduce((acc, plugin) => plugin(acc), nextConfig);
+};
