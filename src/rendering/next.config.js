c7aa81fd58b25ee3276a5cc46c2dc8e90e1d4796
John Montojo <11521672+jjrm@users.noreply.github.com>
2024-05-30 16:24:43 -0400

    Update next.config.js

--- src/rendering/next.config.js
+++ src/rendering/next.config.js
@@ -4,7 +4,7 @@ const plugins = require('./src/temp/next-config-plugins') || {};
 const publicUrl = jssConfig.publicUrl;
 
 // New Relic
-const nrExternals = require('@newrelic/next/load-externals')
+// const nrExternals = require('@newrelic/next/load-externals')
 
 /**
  * @type {import('next').NextConfig}
@@ -83,20 +83,20 @@ const nextConfig = {
       },
     ];
   },
-  experimental: {
-    // Without this setting, the Next.js compilation step will routinely
-    // try to import files such as `LICENSE` from the `newrelic` module.
-    // See https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages.
-    serverComponentsExternalPackages: ['newrelic']
-  },
+  // experimental: {
+    // // Without this setting, the Next.js compilation step will routinely
+    // // try to import files such as `LICENSE` from the `newrelic` module.
+    // // See https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages.
+    // serverComponentsExternalPackages: ['newrelic']
+  // },
 
-  // In order for newrelic to effectively instrument a Next.js application,
-  // the modules that newrelic supports should not be mangled by webpack. Thus,
-  // we need to "externalize" all of the modules that newrelic supports.
-  webpack: (config) => {
-    nrExternals(config)
-    return config
-  }
+  // // In order for newrelic to effectively instrument a Next.js application,
+  // // the modules that newrelic supports should not be mangled by webpack. Thus,
+  // // we need to "externalize" all of the modules that newrelic supports.
+  // webpack: (config) => {
+    // nrExternals(config)
+    // return config
+  // }
 };
 	
 module.exports = () => {
