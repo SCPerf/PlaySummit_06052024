eabf6bae8c3ba31eb169f3f75c8d44852ab3d411
John Montojo <11521672+jjrm@users.noreply.github.com>
2024-05-30 09:13:33 -0400

    Update _document.tsx

--- src/rendering/src/pages/_document.tsx
+++ src/rendering/src/pages/_document.tsx
@@ -7,7 +7,7 @@ type NewRelicProps = {
   browserTimingHeader: string;
 };
 
-class MyDocument extends Document {
+class MyDocument extends Document<NewRelicProps> {
  static async getInitialProps(
    ctx: DocumentContext
  ): Promise<DocumentInitialProps & NewRelicProps> {
@@ -36,9 +36,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-		  <script
-           type="text/javascript"
-           dangerouslySetInnerHTML={{ __html: this.props.browserTimingHeader }}
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{ __html: this.props.browserTimingHeader }}
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
